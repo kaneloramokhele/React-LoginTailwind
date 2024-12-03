3e6ac0c9405596e89ee0f40c31f73dff56eb135d
ramokhele <kaneloramokhele@gmail.com>
2024-12-03 11:19:58 +0200

    Changed the backgound in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
           <Form/>
         </div>
 
-        <div className="hidden lg:flex h-full items-center justify-center bg-gray-200">
-          <div className="w-60 h-60 bg-gradient-to-tr from-slate-900 to-blue-700"/>
+        <div className="hidden lg:flex h-full w-1/2 items-center justify-center bg-gray-200">
+          <div className="w-60 h-60 bg-gradient-to-tr from-slate-900 to-blue-700 rounded-full"/>
         </div>
       </div>
     </>
