ec6390fc151926f635bca8786449330d814e3e07
ramokhele <kaneloramokhele@gmail.com>
2024-12-03 11:50:39 +0200

    MOdified the index.css to make half half

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,9 @@ function App() {
           <Form/>
         </div>
 
-        <div className="hidden lg:flex h-full w-1/2 items-center justify-center bg-gray-200">
-          <div className="w-60 h-60 bg-gradient-to-tr from-slate-900 to-blue-700 rounded-full"/>
+        <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200">
+          <div className="w-60 h-60 bg-gradient-to-tr from-slate-900 to-blue-800 rounded-full animate-bounce"/>
+          <div className="w-full h-1/2 absolute bottom-0 bg-white/1 backdrop-blur-lg"/>
         </div>
       </div>
     </>
