b52dd74499f520d236fd725ac29c58e768d01daf
ramokhele <kaneloramokhele@gmail.com>
2024-12-03 10:50:00 +0200

    Fixed the errors and warnings, changed .js to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import Form from "./components/Form"
 
 
 function App() {
 
   return (
     <>
-      <div>
+      <div className="flex w-full h-screen">
+        <div className="w-full flex items-center justify-center lg:w-1/2">
+          <Form/>
+        </div>
 
+        <div className="hidden lg:flex h-full items-center justify-center bg-gray-200">
+          <div className="w-60 h-60 bg-gradient-to-tr from-slate-900 to-blue-700"/>
+        </div>
       </div>
     </>
   )
---
+++ src/components/Form.jsx
@@ -0,0 +1,10 @@
+// frontend/src/components/Form.js
+
+export default function Form() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      Hello Form
+    </div>
+  );
+}
+
